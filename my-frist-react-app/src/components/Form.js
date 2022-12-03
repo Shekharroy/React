3ecd240ffad50586bfea5eff84038decb41f5543
gic-chandrashekhar <89963131+gic-chandrashekhar@users.noreply.github.com>
2022-12-04 02:43:55 +0530

    Adding fontawsome library and word with charcter count and also preview text and also reading time calculation

--- my-frist-react-app/src/components/Form.js
+++ my-frist-react-app/src/components/Form.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react'
-
+import React, {useState} from 'react' // useState is Hook
+// What is Hooks?
+// Basically Hooks is used for supporting class features without making the class.
 export default function Form(props) {
-    const [text, setText] = useState("Enter text here...");
+    const [text, setText] = useState("Enter text here..."); // Using useState Hook for changing the state;
     const handleOnChangeEvent = ((event) =>{
         setText(event.target.value);
     });
@@ -9,15 +10,29 @@ export default function Form(props) {
        let  newText = text.toUpperCase();
        setText(newText);
     });
+    const handleLowClick = (() =>{
+        let  newText = text.toLowerCase();
+        setText(newText);
+    });
 
   return (
-    <div>
-        <div className="mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={handleOnChangeEvent} id="myform" rows="8"></textarea>
+    <>
+        <div className='container'>
+            <div className="mb-3">
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" value={text} onChange={handleOnChangeEvent} id="myform" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick = {handleUpClick}>Change to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick = {handleLowClick}>Change to Lowercase</button>
         </div>
-        <button className="btn btn-primary" onClick = {handleUpClick}>Change to UpperCase</button>
-    </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
 
