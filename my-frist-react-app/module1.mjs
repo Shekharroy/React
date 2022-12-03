23a7a5ce333462a9a6c66a32c68f88e2f2c9e62c
gic-chandrashekhar <89963131+gic-chandrashekhar@users.noreply.github.com>
2022-12-03 21:00:24 +0530

    Understanding props and propstype in react and make first actual application using react

---
+++ my-frist-react-app/module1.mjs
@@ -0,0 +1,9 @@
+const a ="Chandrashekhar";
+const b ="Shivani";
+const c ="Minakshi";
+const d ="Sabnam Khan";
+
+export default d;
+export {a};
+export {b};
+export {c}
---
+++ my-frist-react-app/module2.mjs
@@ -0,0 +1,5 @@
+import s, {a, b, c} from './module1.mjs'
+console.log(s);
+console.log(a)
+console.log(b)
+console.log(c)
